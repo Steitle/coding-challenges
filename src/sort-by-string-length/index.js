92c1c70603ae5a0d02b44e8e025d6d2496733bba
Matt Steitle <matt.steitle@users.noreply.github.com>
2019-06-19 17:52:24 -0600

    Adding first challenge

---
+++ src/sort-by-string-length/index.js
@@ -0,0 +1,8 @@
+/**
+ * Sorts array of strings by shortest to longest
+ *
+ * @param {Array<string>} strings
+ *
+ * @return {Array<string>}
+ */
+export default function sortByStringLength (strings = [], descending = false) {}
