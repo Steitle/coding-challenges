d848d8a48c96d19e30614133e7d932d676a6fa5f
Matt Steitle <matt.steitle@users.noreply.github.com>
2019-06-19 18:17:54 -0600

    Adding eslint indentation rule 4 spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    "jest/globals": true
-  },
-  plugins: ["jest"],
-  extends: "standard",
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  rules: {}
+    env: {
+        browser: true,
+        es6: true,
+        'jest/globals': true
+    },
+    plugins: ['jest'],
+    extends: 'standard',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    rules: {
+        indent: [
+            'error',
+            4,
+            {
+                SwitchCase: 1
+            }
+        ]
+    }
 };
