e02391719fe9009a1eea2228b1ba012af9a9b5a6
Matt Steitle <matt.steitle@users.noreply.github.com>
2019-06-19 18:19:16 -0600

    Editing test, adding non-mutation test

--- src/sort-strings-by-length/sort-strings-by-length.spec.js
+++ src/sort-strings-by-length/sort-strings-by-length.spec.js
@@ -1,42 +1,47 @@
 import sut from '.'
 
 describe('sortByStringLength', () => {
-  describe.skip('sad path', () => {
-    it('should return an empty array when passed an empty array', () => {
-      const result = sut()
-      const expected = []
+    describe('sad path', () => {
+        it('should return an empty array when passed an empty array', () => {
+            const result = sut([])
+            const expected = []
 
-      expect(result).toEqual(expected)
+            expect(result).toEqual(expected)
+        })
+
+        it('should not mutate the input array', () => {
+            const input = []
+            const result = sut(input)
+
+            expect(result).not.toBe(input)
+        })
+
+        it('should filter out non-strings', () => {
+            const input = [1, 'dork', true, 'hello', false, 'hi', null, 'a', 'pie']
+            const result = sut(input)
+            const expected = ['a', 'hi', 'pie', 'dork', 'hello']
+
+            expect(result).toEqual(expected)
+        })
     })
 
-    it('should filter out non-strings', () => {
-      it('should return an array sorted by length shortest to longest without non-strings', () => {
-        const input = [1, 'dork', true, 'hello', false, 'hi', null, 'a', 'pie']
-        const result = sut(input)
-        const expected = ['a', 'hi', 'pie', 'dork', 'hello']
+    describe('sorting ascending (default)', () => {
+        it('should return an array sorted by length shortest to longest', () => {
+            const input = ['dork', 'hello', 'hi', 'a', 'pie']
+            const result = sut(input)
+            const expected = ['a', 'hi', 'pie', 'dork', 'hello']
 
-        expect(result).toEqual(expected)
-      })
+            expect(result).toEqual(expected)
+        })
     })
-  })
 
-  describe('sorting ascending (default)', () => {
-    it('should return an array sorted by length shortest to longest', () => {
-      const input = ['dork', 'hello', 'hi', 'a', 'pie']
-      const result = sut(input)
-      const expected = ['a', 'hi', 'pie', 'dork', 'hello']
+    describe('sorting descending (optional)', () => {
+        it('should return an array sorted by length shortest to longest', () => {
+            const input = ['dork', 'hello', 'hi', 'a', 'pie']
+            const result = sut(input, true)
+            const expected = ['hello', 'dork', 'pie', 'hi', 'a']
 
-      expect(result).toEqual(expected)
+            expect(result).toEqual(expected)
+        })
     })
-  })
-
-  describe.skip('sorting descending (optional)', () => {
-    it('should return an array sorted by length shortest to longest', () => {
-      const input = ['dork', 'hello', 'hi', 'a', 'pie']
-      const result = sut(input, true)
-      const expected = ['hello', 'dork', 'pie', 'hi', 'a']
-
-      expect(result).toEqual(expected)
-    })
-  })
 })
