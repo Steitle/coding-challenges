dda8b948a3d4df447292e9617038866f58d30bbc
Matt Steitle <matt.steitle@users.noreply.github.com>
2019-06-20 17:13:57 -0600

    Adding check for equal length words

--- src/sort-strings-by-length/sort-strings-by-length.spec.js
+++ src/sort-strings-by-length/sort-strings-by-length.spec.js
@@ -1,6 +1,26 @@
 import sut from '.'
 
 describe('sortByStringLength', () => {
+    describe('sorting ascending (default)', () => {
+        it('should return an array sorted by length shortest to longest', () => {
+            const input = ['dork', 'dog', 'hello', 'hi', 'a', 'pie']
+            const result = sut(input)
+            const expected = ['a', 'hi', 'dog', 'pie', 'dork', 'hello']
+
+            expect(result).toEqual(expected)
+        })
+    })
+
+    describe('sorting descending (optional)', () => {
+        it('should return an array sorted by length shortest to longest', () => {
+            const input = ['dork', 'dog', 'hello', 'hi', 'a', 'pie']
+            const result = sut(input, true)
+            const expected = ['hello', 'dork', 'dog', 'pie', 'hi', 'a']
+
+            expect(result).toEqual(expected)
+        })
+    })
+
     describe('sad path', () => {
         it('should return an empty array when passed an empty array', () => {
             const result = sut([])
@@ -17,29 +37,9 @@ describe('sortByStringLength', () => {
         })
 
         it('should filter out non-strings', () => {
-            const input = [1, 'dork', true, 'hello', false, 'hi', null, 'a', 'pie']
+            const input = [1, 'dork', true, 'hello', false, 'hi', null, 'dog', 'a', 'pie']
             const result = sut(input)
-            const expected = ['a', 'hi', 'pie', 'dork', 'hello']
-
-            expect(result).toEqual(expected)
-        })
-    })
-
-    describe('sorting ascending (default)', () => {
-        it('should return an array sorted by length shortest to longest', () => {
-            const input = ['dork', 'hello', 'hi', 'a', 'pie']
-            const result = sut(input)
-            const expected = ['a', 'hi', 'pie', 'dork', 'hello']
-
-            expect(result).toEqual(expected)
-        })
-    })
-
-    describe('sorting descending (optional)', () => {
-        it('should return an array sorted by length shortest to longest', () => {
-            const input = ['dork', 'hello', 'hi', 'a', 'pie']
-            const result = sut(input, true)
-            const expected = ['hello', 'dork', 'pie', 'hi', 'a']
+            const expected = ['a', 'hi', 'dog', 'pie', 'dork', 'hello']
 
             expect(result).toEqual(expected)
         })
