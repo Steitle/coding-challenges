b3ae39b33a3b90b13fb3bce56f4d4041798cd320
Matt Steitle <matt.steitle@users.noreply.github.com>
2019-06-19 18:08:01 -0600

    Removing old dir

--- src/sort-by-string-length/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Sorts array of strings by shortest to longest
- *
- * @param {Array<string>} strings
- *
- * @return {Array<string>}
- */
-export default function sortStringsByLength (strings = [], descending = false) {}
